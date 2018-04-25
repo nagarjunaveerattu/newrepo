d408b754223ce94610966accbeeffb88e71cc3a0
Maktron-8 <Maktron-8@MAKTRON-17-PC>
2018-04-25 15:18:55 +0530

    Add project files.

---
+++ sample login/Controllers/loginController.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sample_login.Models;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Core.Metadata.Edm;
+
+namespace sample_login.Controllers
+{
+    public class loginController : Controller
+    {
+        [HttpGet]
+        public ActionResult createuser(int id = 0)
+        {
+            login obj = new login();
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult createuser(login obj)
+        {
+            using (maktronEntities mn=new maktronEntities())
+            {
+                mn.logins.Add(obj);
+                mn.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.sucessfullmessage = "welcome to login.";
+            return View("createuser", new login());
+
+
+        }
+    }
+}
+    
---
+++ sample login/Models/register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sample_login.Models
+{
+    public class register
+    {
+        public string userid { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+
+        public string password { get; set; }
+
+        public string confrompassword { get; set; }
+
+    }
+}
